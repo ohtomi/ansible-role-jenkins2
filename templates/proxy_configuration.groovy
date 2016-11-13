858224be7a48e2c00ffbaf36527fe16cfeba2297
ohtomi <ohtomi.kenichi@gmail.com>
2016-11-14 00:01:23 +0900

    Add role variables for /etc/sysconfig/jenkins

--- templates/proxy_configuration.groovy
+++ templates/proxy_configuration.groovy
@@ -4,7 +4,11 @@ def username = "{{ jenkins2_proxy_username }}"
 def password = "{{ jenkins2_proxy_password }}"
 def noProxyHosts = "{{ jenkins2_proxy_no_proxy_hosts }}"
 
-def proxy = new hudson.ProxyConfiguration(host, port, username, password, noProxyHosts)
-proxy.save()
-
-jenkins.model.Jenkins.instance.proxy = proxy
+if (host != "") {
+    def proxy = new hudson.ProxyConfiguration(host, port, username, password, noProxyHosts)
+    proxy.save()
+    jenkins.model.Jenkins.instance.proxy = proxy
+} else {
+    jenkins.model.Jenkins.instance.proxy = null
+    jenkins.model.Jenkins.instance.save()
+}
